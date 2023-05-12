d417e7465e0746b91326ff8168bf0f4b620eadb6
Nick Morales <ngmorales97@gmail.com>
2023-05-12 02:07:36 -0500

    Added cmake function for adding an integration test

---
+++ catch2_ros/cmake/catch2_ros_add_integration_test.cmake
@@ -0,0 +1,39 @@
+function(catch2_ros_add_integration_test testname)
+  cmake_parse_arguments(ARG
+    "SKIP_TEST"
+    "LAUNCH_FILE;TIMEOUT;RESULT_FILE;RESULT_FILE_PARAMETER_NAME"
+    ""
+    ${ARGN})
+  if(ARG_UNPARSED_ARGUMENTS)
+    message(FATAL_ERROR "catch2_ros_add_integration_test() called with unused arguments: "
+      "${ARG_UNPARSED_ARGUMENTS}")
+  endif()
+
+  if(NOT ${ament_cmake_FOUND})
+    find_package(ament_cmake REQUIRED)
+  endif()
+
+  if(NOT ARG_LAUNCH_FILE)
+    message(FATAL_ERROR
+    "catch2_ros_add_integration_test() must be invoked with the LAUNCH_FILE argument")
+  endif()
+  if(NOT ARG_RESULT_FILE)
+    set(ARG_RESULT_FILE "${AMENT_TEST_RESULTS_DIR}/${PROJECT_NAME}/${testname}.xml")
+  endif()
+  if(NOT ARG_TIMEOUT)
+    set(ARG_TIMEOUT 60)
+  endif()
+  if(NOT ARG_TIMEOUT GREATER 0)
+    message(FATAL_ERROR "catch2_ros_add_integration_test() the TIMEOUT argument must be a "
+      "valid number and greater than zero")
+  endif()
+  if(NOT ARG_RESULT_FILE_PARAMETER_NAME)
+    set(ARG_RESULT_FILE_PARAMETER_NAME "result_file")
+  endif()
+
+  ament_add_test(${testname}
+    RESULT_FILE ${ARG_RESULT_FILE}
+    COMMAND ros2 launch ${PROJECT_NAME} ${ARG_LAUNCH_FILE} ${ARG_RESULT_FILE_PARAMETER_NAME}:=${ARG_RESULT_FILE}
+    RUNNER ${catch2_ros_DIR}/../scripts/run_test.py
+  )
+endfunction()
--- catch2_ros/include/catch2_ros/arguments.hpp
+++ catch2_ros/include/catch2_ros/arguments.hpp
@@ -8,65 +8,64 @@
 
 namespace catch2_ros
 {
-  /// @brief A class for generating simulated argc/argv values from an input string
-  /// https://stackoverflow.com/questions/39883433/create-argc-argv-in-the-code
-  /// TODO - improve so this can handle quotations in the input string properly
-  class SimulateArgs
-  {
-  public:
-    /// @brief Parse string into data structures that can be used to generate simulated argc/argvs
-    /// @param args - command string to parse into arguments
-    SimulateArgs(const std::string & args);
+/// @brief A class for generating simulated argc/argv values from an input string
+/// https://stackoverflow.com/questions/39883433/create-argc-argv-in-the-code
+/// TODO - improve so this can handle quotations in the input string properly
+class SimulateArgs
+{
+public:
+  /// @brief Parse string into data structures that can be used to generate simulated argc/argvs
+  /// @param args - command string to parse into arguments
+  SimulateArgs(const std::string & args);
 
-    /// @brief generate argc
-    /// @return argc, argument count
-    int argc() const;
+  /// @brief generate argc
+  /// @return argc, argument count
+  int argc() const;
 
-    /// @brief generate argv
-    /// @return pointer to first argument data
-    const char* const * argv() const;
+  /// @brief generate argv
+  /// @return pointer to first argument data
+  const char * const * argv() const;
 
-  private:
-    /// @brief string argument data
-    std::vector<std::string> args_ {};
+private:
+  /// @brief string argument data
+  std::vector<std::string> args_ {};
 
-    /// @brief pointers to string argument data
-    std::vector<char*> argv_vec_ {};
-  };
+  /// @brief pointers to string argument data
+  std::vector<char *> argv_vec_ {};
+};
 
-  /// @brief a class for splitting ROS arguments out of input arguments
-  class SplitROSArgs
-  {
-  public:
-    
-    /// @brief find where ROS Args start
-    /// @param argc - argument count
-    /// @param argv - pointer to first argument data
-    /// TODO improve so the end of ROS args can also be detected (--)
-    SplitROSArgs(const int argc, const char* const* argv);
+/// @brief a class for splitting ROS arguments out of input arguments
+class SplitROSArgs
+{
+public:
+  /// @brief find where ROS Args start
+  /// @param argc - argument count
+  /// @param argv - pointer to first argument data
+  /// TODO improve so the end of ROS args can also be detected (--)
+  SplitROSArgs(const int argc, const char * const * argv);
 
-    /// @brief return original argc
-    /// @return original argc
-    int argc() const;
+  /// @brief return original argc
+  /// @return original argc
+  int argc() const;
 
-    /// @brief return argc if ROS args are taken out
-    /// @return argc if ROS args are taken out
-    int argc_without_ros() const;
+  /// @brief return argc if ROS args are taken out
+  /// @return argc if ROS args are taken out
+  int argc_without_ros() const;
 
-    /// @brief return original argv, can also be used when ROS args are taken out
-    /// @return original argv
-    const char* const* argv() const;
+  /// @brief return original argv, can also be used when ROS args are taken out
+  /// @return original argv
+  const char * const * argv() const;
 
-  private:
-    /// @brief original argc
-    const int argc_;
+private:
+  /// @brief original argc
+  const int argc_;
 
-    /// @brief original argv
-    const char* const* argv_;
+  /// @brief original argv
+  const char * const * argv_;
 
-    /// @brief the index where ros args start
-    int ndx_ros_args_start_;
-  };
+  /// @brief the index where ros args start
+  int ndx_ros_args_start_;
+};
 }
 
 #endif
