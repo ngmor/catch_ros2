3f5a3e7d04fc1a384f4b04b130cbc4dc832a4605
Nick Morales <ngmorales97@gmail.com>
2023-06-20 23:12:17 -0500

    Made custom main test unique

--- catch_ros2/examples/custom_main/example_test_with_custom_main.cpp
+++ catch_ros2/examples/custom_main/example_test_with_custom_main.cpp
@@ -12,84 +12,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// @file - Example unit tests using catch_ros2 utilities.
+/// @file - Example test with a custom main
 
 #include "catch_ros2/catch_ros2.hpp"
-#include "rclcpp/rclcpp.hpp"
 
-using catch_ros2::SimulateArgs;
+using catch_ros2::tokenize;
 
-TEST_CASE("parameters", "[parameters]") {
-  // The SimulateArgs class can be used to synthesize input arguments for rclcpp::init()
-  const auto args = SimulateArgs{"/fake/path --ros-args -p param1:=-1.4 -p param3:=14"};
+TEST_CASE("tokenize", "[tokenize]") {
 
-  // Initialize ROS with simulated arguments
-  rclcpp::init(args.argc(), args.argv());
+  SECTION("no quotes") {
+    const auto result = tokenize("tokenize me please!");
+    CHECK(result.at(0) == "tokenize");
+    CHECK(result.at(1) == "me");
+    CHECK(result.at(2) == "please!");
+  }
 
-  // Init test node
-  auto node = rclcpp::Node::make_shared("test_node");
-
-  // Test that parameters are received as expected by the node
-  node->declare_parameter<double>("param1");
-  node->declare_parameter<double>("param2", 3.45);
-  node->declare_parameter<int>("param3", 2);
-
-
-  // Assertions
-  // Should be set to simulated parameter and not throw
-  CHECK(node->get_parameter("param1").get_parameter_value().get<double>() == -1.4);
-
-  // Should be set to default value
-  CHECK(node->get_parameter("param2").get_parameter_value().get<double>() == 3.45);
-
-  // Should be set to simulated parameter, not default value
-  CHECK(node->get_parameter("param3").get_parameter_value().get<int>() == 14);
-
-  // Should throw since it was never initialized
-  CHECK_THROWS(node->declare_parameter<bool>("param4"));
-
-  // Shutdown ROS
-  rclcpp::shutdown();
+  SECTION("quotes") {
+    const auto result = tokenize("here is a string \"with some\" quotations \"in it\"");
+    CHECK(result.at(0) == "here");
+    CHECK(result.at(1) == "is");
+    CHECK(result.at(2) == "a");
+    CHECK(result.at(3) == "string");
+    CHECK(result.at(4) == "with some");
+    CHECK(result.at(5) == "quotations");
+    CHECK(result.at(6) == "in it");
+  }
 }
 
-TEST_CASE("complex parameters", "[parameters]") {
-  // The SimulateArgs class can also be used with more complex parameters included quotation marks
-  const auto args = SimulateArgs{
-    "/fake/path --ros-args -p param1:=1 "
-    "-p param2:=\"[2.0, 3.0, 4.0]\" -p param3:=\"/path/with spaces/\""
-  };
+// Example main can be specified like so. For more information see
+// https://github.com/catchorg/Catch2/blob/devel/docs/own-main.md
+int main( int argc, char* argv[] ) {
 
-  // Initialize ROS with simulated arguments
-  rclcpp::init(args.argc(), args.argv());
+  // your setup ...
 
-  // Init test node
-  auto node = rclcpp::Node::make_shared("test_node");
+  int result = Catch::Session().run( argc, argv );
 
-  // Test that parameters are received as expected by the node
-  node->declare_parameter<int>("param1");
-  node->declare_parameter<std::vector<double>>("param2");
-  node->declare_parameter<std::string>("param3", "default value");
+  // your clean-up...
 
-
-  // Assertions
-  // Should be set to simulated parameter and not throw
-  CHECK(node->get_parameter("param1").get_parameter_value().get<int>() == 1);
-  // Should be properly read as a list
-  const auto param2 =
-    node->get_parameter("param2").get_parameter_value().get<std::vector<double>>();
-  CHECK(param2.at(0) == 2.0);
-  CHECK(param2.at(1) == 3.0);
-  CHECK(param2.at(2) == 4.0);
-
-  // Should be set to simulated parameter, not default value
-  CHECK(
-    node->get_parameter(
-      "param3").get_parameter_value().get<std::string>() == "/path/with spaces/");
-
-  // Shutdown ROS
-  rclcpp::shutdown();
-}
-
-int main (int argc, char * argv[]) {
-    return Catch::Session().run( argc, argv );
+  return result;
 }
